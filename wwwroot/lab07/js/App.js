0d687e6d372c2726f37a67daa27022ece1c6a496
Simona Colapicchioni <s.colapicchioni@gmail.com>
2016-09-23 10:55:03 +0200

    Lab07 - LocalStorage

---
+++ wwwroot/lab07/js/App.js
@@ -0,0 +1,5 @@
+import {Trip} from "./Trip";
+import {LocalStorageDataLayer} from "./LocalStorageDataLayer";
+import {ViewModel} from "./ViewModel";
+
+const vm = new ViewModel(new LocalStorageDataLayer());
---
+++ wwwroot/lab07/js/Trip.js
@@ -0,0 +1,45 @@
+export class Trip{
+    
+    get description() {
+        return this._description;
+    }
+    set description(v) {
+        this._description = v;
+    }
+    
+    get destination(){
+        return this._destination;
+    }
+    set destination(v) {
+        this._destination = v;
+    }
+    
+    get date() {
+        return this._date;
+    }
+    set date(v) {
+        this._date = v;
+    }
+    
+    get rating(){
+        return this._rating;
+    }
+    set rating(v) {
+        this._rating = v;
+    }
+    
+    get id() {
+        return this._id;
+    }
+    set id(v) {
+        this._id = v;
+    }
+
+    constructor(id=0, destination="", description="", date="", rating=0){
+        this.id = id || 0;
+        this.destination = destination || "";
+        this.description = description || "";
+        this.date = date || "";
+        this.rating = rating || 6;
+    }
+}
